dbda4cdf539da6f911fd4159ae4684dc677ec645
ani <anitha.s.goutham@gmail.com>
2024-11-08 14:32:46 +0800

    k8-resources

--- workstation.sh
+++ workstation.sh
@@ -37,10 +37,6 @@ chmod +x ./kubectl
 mv kubectl /usr/local/bin/kubectl
 VALIDATE $? "kubectl installation"
 
-# # kubens
-# git clone https://github.com/ahmetb/kubectx /opt/kubectx
-# ln -s /opt/kubectx/kubens /usr/local/bin/kubens
-# VALIDATE $? "kubens installation"
 
 
 # Helm
