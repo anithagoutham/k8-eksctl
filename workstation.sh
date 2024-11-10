4335912abccd19bab7e50ebc58785754eec4db15
ani <anitha.s.goutham@gmail.com>
2024-11-10 21:12:32 +0800

    k8

--- workstation.sh
+++ workstation.sh
@@ -30,6 +30,19 @@ else
     echo "You are super user."
 fi
 
+# eksctl
+
+ARCH=amd64
+PLATFORM=$(uname -s)_$ARCH
+
+curl -sLO "https://github.com/eksctl-io/eksctl/releases/latest/download/eksctl_$PLATFORM.tar.gz"
+
+tar -xzf eksctl_$PLATFORM.tar.gz -C /tmp && rm eksctl_$PLATFORM.tar.gz
+
+sudo mv /tmp/eksctl /usr/local/bin
+
+
+
 
 # kubectl
 curl -O https://s3.us-west-2.amazonaws.com/amazon-eks/1.31.0/2024-09-12/bin/linux/amd64/kubectl
